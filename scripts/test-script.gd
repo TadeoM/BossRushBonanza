230ed8cfdde67ce1ab364b68322fd36941e03395
Tadeo <tmenichel@protonmail.com>
2023-10-10 16:34:28 -0400

    test -script merge with luck

--- scripts/test-script.gd
+++ scripts/test-script.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var somethingCool = 5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
