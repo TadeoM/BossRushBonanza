afd7144d23cbc997bd6244a054530d89554d61b7
Tadeo <tmenichel@protonmail.com>
2023-10-10 22:05:13 -0400

    move enemy, simple script

--- scripts/mobs/ai_enemy_movement.gd
+++ scripts/mobs/ai_enemy_movement.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
-@export var speed = 400
+@export var speed = 30
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
 	_animated_sprite.play("default")
+
 
 func _physics_process(delta):
 	get_input()
