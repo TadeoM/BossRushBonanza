c74f2d6059ea5232e742d48d86e957bb71375d20
Tadeo <tmenichel@protonmail.com>
2023-11-01 17:30:04 -0400

    a* start

--- scripts/mobs/enemy_a_star.gd
+++ scripts/mobs/enemy_a_star.gd
@@ -1,12 +1,11 @@
-extends Camera2D
+extends Node
 
-var player : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = $"../Player"
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = player.position
+	pass
